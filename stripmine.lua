aac8c4762d7a95760358f097b5977004eba62d0c
Keith Thibodeaux <keith@railscoder.net>
2023-12-02 13:35:14 -0500

    reorganize stripmine

--- stripmine.lua
+++ stripmine.lua
@@ -4,18 +4,17 @@ local args = {...}
 local tunnels = args[1]
 local tunnelDepth = args[2] or 5
 
-function strip()
+for j = 1, tunnels, 1 do
+  kt.digTunnel(3, true, true)
+
   turtle.turnLeft()
   kt.digTunnel(tunnelDepth, true, true)
   kt.turnAround()
   kt.forward(tunnelDepth)
+
   kt.digTunnel(tunnelDepth, true, true)
   kt.turnAround()
   kt.forward(tunnelDepth)
-  turtle.turnRight()
-end
 
-for j = 1, tunnels, 1 do
-  kt.digTunnel(3, true, true)
-  strip()
+  turtle.turnRight()
 end
